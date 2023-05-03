e81b0bc354a69a3fe96889cf1d848e6fc08acddb
barram19 <131893869+barram19@users.noreply.github.com>
2023-05-03 15:04:30 -0500

    Create nbaodds.py

---
+++ nbaodds.py
@@ -0,0 +1,20 @@
+!pip install requests
+
+
+# Set API endpoint URL and API key
+
+url = 'https://api.the-odds-api.com/v4/sports/basketball_nba/odds/?apiKey=8f283a0b9aa2f6e2b517c77b786bb1cb&bookmakers=fanduel&markets=h2h,spreads&oddsFormat=american'
+api_key = '8f283a0b9aa2f6e2b517c77b786bb1cb'
+
+
+# Set headers for the request
+headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': api_key,
+}
+
+# Make the request and get the response data
+response = requests.get(url, params=parameters, headers=headers)
+data = response.json()
+print(data[0]) # Access the first element of the list
+nba_odds = (data[0])
